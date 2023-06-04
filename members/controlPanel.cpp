16bd5da3698abfce804a4146bd88e871450914a0
apozinn <samuelsoaresmonteiro12@gmail.com>
2023-06-04 20:25:53 -0300

    added some menus to control panel(beta)

--- members/controlPanel.cpp
+++ members/controlPanel.cpp
@@ -1,4 +1,12 @@
+struct ControlMenu {
+	const char* name;
+	const char* shortkut;
+	int ID;
+};
+
 class ControlPanel : public wxPanel {
+	wxScrolled<wxPanel>* menus_container;
+	wxPanel* selectedMenu;
 public:
 	ControlPanel(wxFrame* parent, wxWindowID ID) : wxPanel(
 		parent, ID, wxPoint(parent->GetSize().GetWidth()/2-225, 50), wxSize(450, 250)
@@ -25,13 +33,46 @@ public:
 		top_container->SetSizerAndFit(top_ctn_sizer);
 		sizer->Add(top_container, 0, wxEXPAND);
 
-		wxAcceleratorEntry entries[1];
-	    entries[0].Set(wxACCEL_NORMAL , WXK_ESCAPE, ID_EXIT_CONTROL_PANEL);
-	    wxAcceleratorTable accel(1, entries);
+		menus_container = new wxScrolled<wxPanel>(this);
+		wxBoxSizer* menu_ctn_sizer = new wxBoxSizer(wxVERTICAL);
+
+		for(const auto& ctrl_menu : menus) {
+			wxPanel* menu = new wxPanel(menus_container);
+			if(ctrl_menu.ID == 1) {
+				menu->SetBackgroundColour(wxColor(56, 56, 56));
+				selectedMenu = menu;
+			}
+			wxBoxSizer* menu_sizer = new wxBoxSizer(wxHORIZONTAL);
+			
+			wxStaticText* menu_name = new wxStaticText(menu, wxID_ANY, ctrl_menu.name);
+			wxFont menu_name_font(10, wxFONTFAMILY_MODERN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+	        menu_name_font.SetFaceName(wxT("Monospace"));
+	        menu_name->SetFont(menu_name_font);
+			menu_sizer->Add(menu_name, 1, wxEXPAND | wxALL, 5);
+
+	        wxStaticText* menu_shortkut = new wxStaticText(menu, wxID_ANY, ctrl_menu.shortkut);
+			wxFont menu_shortkut_font(7, wxFONTFAMILY_MODERN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+	        menu_shortkut_font.SetFaceName(wxT("Monospace"));
+	        menu_shortkut->SetFont(menu_shortkut_font);
+			menu_sizer->Add(menu_shortkut, 0, wxEXPAND | wxTOP, 8);
+
+			menu->SetSizerAndFit(menu_sizer);
+			menu_ctn_sizer->Add(menu, 0, wxEXPAND | wxALL, 3);
+		}
+
+		menus_container->SetSizerAndFit(menu_ctn_sizer);
+		menus_container->FitInside();
+    	menus_container->SetScrollRate(20, 20);
+		sizer->Add(menus_container, 1, wxEXPAND);
+
+		wxAcceleratorEntry entries[3];
+	    entries[0].Set(wxACCEL_NORMAL, WXK_ESCAPE, ID_EXIT_CONTROL_PANEL);
+	    entries[1].Set(wxACCEL_NORMAL, WXK_UP, ID_CONTROL_PANEL_UP);
+	    entries[2].Set(wxACCEL_NORMAL, WXK_DOWN, ID_CONTROL_PANEL_DOWN);
+	    wxAcceleratorTable accel(3, entries);
 	    SetAcceleratorTable(accel);
 
 	    SetSizerAndFit(sizer);
-
 	    SetMinSize(wxSize(450, 250));
 		SetSize(wxSize(450, 250));
 	}
@@ -39,11 +80,54 @@ public:
 		FindWindowById(ID_STATUS_BAR)->SetFocus();
 		Destroy();
 	}
+	void UpSelection(wxCommandEvent& event) {
+		bool finded;
+		for(auto&& menu : menus_container->GetChildren()) {
+			if(finded) return;
+			if(menu == selectedMenu) {
+				auto next_menu = menu->GetPrevSibling();
+				if(!next_menu) {
+					next_menu = menus_container->GetChildren()[0];
+				}
+
+				if(next_menu != menu) {
+					next_menu->SetBackgroundColour(wxColor(56, 56, 56));
+					menu->SetBackgroundColour(wxColor(36, 36, 36));
+					selectedMenu = ((wxPanel*)next_menu);
+					finded = true;
+				}
+			}
+		}
+	}
+	void DownSelection(wxCommandEvent& event) {
+		bool finded;
+		for(auto&& menu : menus_container->GetChildren()) {
+			if(finded) return;
+			if(menu == selectedMenu) {
+				auto next_menu = menu->GetNextSibling();
+				if(!next_menu) {
+					next_menu = menus_container->GetChildren()[0];
+				}
+
+				next_menu->SetBackgroundColour(wxColor(56, 56, 56));
+				menu->SetBackgroundColour(wxColor(36, 36, 36));
+				selectedMenu = ((wxPanel*)next_menu);
+				finded = true;
+			}
+		}
+	}
 private:
+	std::vector<ControlMenu> menus {
+		{"Enter Focus Mode", "Shift+F11", 1},
+		{"Hide Side Navigation", "", 2},
+		{"Extensions: Install", "", 3},
+	};
 	wxDECLARE_NO_COPY_CLASS(ControlPanel);
     wxDECLARE_EVENT_TABLE();
 };
 
 wxBEGIN_EVENT_TABLE(ControlPanel, wxPanel)
     EVT_MENU(ID_EXIT_CONTROL_PANEL, ControlPanel::Close)
+    EVT_MENU(ID_CONTROL_PANEL_UP, ControlPanel::UpSelection)
+    EVT_MENU(ID_CONTROL_PANEL_DOWN, ControlPanel::DownSelection)
 wxEND_EVENT_TABLE()
