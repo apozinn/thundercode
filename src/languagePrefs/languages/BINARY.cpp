9c46ed1516b3021ac197d10d2698802053f73715
apozinn <samuelsoaresmonteiro12@gmail.com>
2023-10-18 22:31:43 -0300

    change cmake imports

--- src/languagePrefs/languages/BINARY.cpp
+++ src/languagePrefs/languages/BINARY.cpp
@@ -11,7 +11,7 @@ const LanguageInfo BINARY_PREFS = {
         {mySTC_TYPE_COMMENT_LINE, nullptr},
         {mySTC_TYPE_COMMENT_DOC, nullptr},
         {mySTC_TYPE_NUMBER, nullptr},
-        {mySTC_TYPE_WORD1, nullptr}, // KEYWORDS
+        {mySTC_TYPE_WORD1, nullptr},
         {mySTC_TYPE_STRING, nullptr},
         {mySTC_TYPE_CHARACTER, nullptr},
         {mySTC_TYPE_UUID, nullptr},
@@ -19,12 +19,12 @@ const LanguageInfo BINARY_PREFS = {
         {mySTC_TYPE_OPERATOR, nullptr},
         {mySTC_TYPE_IDENTIFIER, nullptr},
         {mySTC_TYPE_STRING_EOL, nullptr},
-        {mySTC_TYPE_DEFAULT, nullptr}, // VERBATIM
+        {mySTC_TYPE_DEFAULT, nullptr}
         {mySTC_TYPE_REGEX, nullptr},
-        {mySTC_TYPE_COMMENT_SPECIAL, nullptr}, // DOXY
-        {mySTC_TYPE_WORD2, nullptr}, // EXTRA WORDS
-        {mySTC_TYPE_WORD3, nullptr}, // DOXY KEYWORDS
-        {mySTC_TYPE_ERROR, nullptr}, // KEYWORDS ERROR
+        {mySTC_TYPE_COMMENT_SPECIAL, nullptr},
+        {mySTC_TYPE_WORD2, nullptr},
+        {mySTC_TYPE_WORD3, nullptr},
+        {mySTC_TYPE_ERROR, nullptr},
         {-1, nullptr},
         {-1, nullptr},
         {-1, nullptr},
